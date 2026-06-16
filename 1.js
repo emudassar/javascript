43e22303d5a8fa679a3183442b46763e80982719
M Mudassar <contacmudassar@gmail.com>
2026-06-16 09:41:55 +0500

    practicing js

--- 1.js
+++ 1.js
@@ -1,8 +1,14 @@
-function test() {
-  if (true) {
-    var x = 10;   
-    let y = 20;   
-  }
-  console.log(x); // 10 ✅
-  console.log(y); // ReferenceError ❌
-}
+// function test() {
+//   if (true) {
+//     var x = 10;   
+//     let y = 20;   
+//   }
+//   console.log(x); // 10 ✅
+//   console.log(y); // ReferenceError ❌
+// }
+
+console.log(a); // undefined (var is hoisted + initialized)
+console.log(b); // ReferenceError (let is in TDZ)
+
+var a = 5;
+let b = 10;
---
+++ 2.js
@@ -0,0 +1,12 @@
+function outer(){
+  let message = "Hello"
+
+  function inner(){
+    console.log(message);
+    
+  }
+  return inner
+}
+
+const myFunc = outer()
+myFunc()
